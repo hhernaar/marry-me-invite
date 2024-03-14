0676502fe52dcf170e31e18f8287c31fcaafc4a7
hhernaar <hhernaar@gmail.com>
2024-03-13 18:20:40 -0600

    responsive design

--- src/components/PhotoDump.jsx
+++ src/components/PhotoDump.jsx
@@ -7,24 +7,28 @@ const backgroundStyle = {
 
 export const PhotoDump = () => {
   return (
-    <section id='photo-dump-section' style={backgroundStyle} className='pt-3 pb-3'>
-      <div id='photo-dump-info' className='me-3 mb-1'>
+    <section id='photo-dump-section' className='pt-3 pb-3'>
+      <div style={backgroundStyle}>
 
-        <div className='photo-dump-header pt-1 pb-1'>
-          <h3 className='font-el-messiri mb-0'>¡Envíanos tus fotos!</h3>
-        </div>
+        <div id='photo-dump-info' className='me-3 mb-1'>
 
-        <div className='photo-dump-body'>
+          <div className='photo-dump-header pt-1 pb-1'>
+            <h3 className='font-el-messiri mb-0'>¡Envíanos tus fotos!</h3>
+          </div>
 
-          <span className='mb-1'>DA CLICK AQUI</span>
-          <div className='dresscode-body-icons pulseBtn'>
+          <div className='photo-dump-body'>
+
+            <span className='mb-1'>DA CLICK AQUI</span>
+            <div className='dresscode-body-icons pulseBtn'>
 
 
-            <a className='' href='https://wa.me/525525253792?text=¡Hola! Éstas son las fotos que tome durante la fiesta:'>
-              <span className='icon-sm'>
-                <img src={whatsAppIcon} />
-              </span>
-            </a>
+              <a className='' href='https://wa.me/525525253792?text=¡Hola! Éstas son las fotos que tome durante la fiesta:'>
+                <span className='icon-sm'>
+                  <img src={whatsAppIcon} />
+                </span>
+              </a>
+            </div>
+
           </div>
 
         </div>
